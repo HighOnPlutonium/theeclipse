849cd3430b9e3653e57232580b8c6a6d82d63d6e
HighOnPlutonium <74098648+HighOnPlutonium@users.noreply.github.com>
2021-07-22 17:35:34 +0200

    Update TheEclipse.java

--- src/main/java/joelkral/theeclipse/TheEclipse.java
+++ src/main/java/joelkral/theeclipse/TheEclipse.java
@@ -3,7 +3,6 @@ package joelkral.theeclipse;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.PotionItem;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
