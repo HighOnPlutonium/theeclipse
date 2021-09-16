75faa2b11c00908a3bb3eb75abdf176f512a4d4b
HighOnPlutonium <74098648+HighOnPlutonium@users.noreply.github.com>
2021-09-16 08:13:21 +0200

    did weird stuff

--- src/main/java/joelkral/theeclipse/core/init/BlockInit.java
+++ src/main/java/joelkral/theeclipse/core/init/BlockInit.java
@@ -1,7 +1,6 @@
 package joelkral.theeclipse.core.init;
 
 import joelkral.theeclipse.TheEclipse;
-import joelkral.theeclipse.common.blocks.WeirdFurnaceBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.fml.RegistryObject;
@@ -12,5 +11,5 @@ public class BlockInit
 {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TheEclipse.MOD_ID);
 	
-	public static final RegistryObject<Block> WEIRD_FURNACE = BLOCKS.register("weird_furnace", () -> new WeirdFurnaceBlock(Block.Properties.copy(Blocks.FURNACE)));
+	//public static final RegistryObject<Block> WEIRD_FURNACE = BLOCKS.register("weird_furnace", () -> new WeirdFurnaceBlock(Block.Properties.copy(Blocks.FURNACE)));
 }
